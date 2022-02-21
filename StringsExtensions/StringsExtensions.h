b699676076901695cb48b8e677f465f91f90ffb5
Domenico Gonnelli <domenico.gonnelli@nttdata.com>
2022-02-21 10:49:41 +0100

    initial commit

---
+++ StringsExtensions/StringsExtensions.h
@@ -0,0 +1,18 @@
+//
+//  StringsExtensions.h
+//  StringsExtensions
+//
+//  Created by Gonnelli Domenico on 21/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StringsExtensions.
+FOUNDATION_EXPORT double StringsExtensionsVersionNumber;
+
+//! Project version string for StringsExtensions.
+FOUNDATION_EXPORT const unsigned char StringsExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringsExtensions/PublicHeader.h>
+
+
